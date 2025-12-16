b0c70b3d8097af29ab110380be13f3bfc3d0a40a
heberthysamir <heberthy.samir@aluno.ufca.edu.br>
2025-12-16 12:47:20 -0300

    Atualização do pytest

--- Tests/test_subclasses.py
+++ Tests/test_subclasses.py
@@ -1,24 +1,13 @@
-from Playxel import jogo_from_dict, JogoPC
+from jogo import JogoPC, JogoConsole, JogoMobile
 
-def test_converter_pc():
-    data = {
-        "tipo": "JogoPC",
-        "extra": "Steam",
-        "nome": "Portal",
-        "genero": "Puzzle",
-        "plataforma": "pc",
-        "horas_jogadas": 10,
-        "status": "jogando",
-        "data_inicio": "01/01/2024",
-        "data_termino": "",
-        "ano_lancamento": 2007,
-        "avaliacao": 9,
-        "multiplayer": False
-    }
-
-    j = jogo_from_dict(data)
-
-    assert isinstance(j, JogoPC)
+def test_jogo_pc_extra():
+    j = JogoPC("Steam", "CS2", "FPS", "pc", 10, 1, "", "", 2023, 9, False)
     assert j.launcher == "Steam"
-    assert j.nome == "Portal"
-    assert j.status == "jogando"
+
+def test_jogo_console_extra():
+    j = JogoConsole("PS5", "God of War", "Ação", "console", 20, 1, "", "", 2022, 10, False)
+    assert j.console == "PS5"
+
+def test_jogo_mobile_extra():
+    j = JogoMobile("Android", "Clash Royale", "Estrategia", "mobile", 5, 1, "", "", 2016, 8, True)
+    assert j.sistema == "Android"
